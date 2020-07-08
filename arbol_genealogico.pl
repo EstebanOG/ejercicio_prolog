1cdc3544978ff14a6d22ccfffeed6689b63aafea
Esteban <juanesteban199911@gmail.com>
2020-07-08 16:53:16 -0500

    Árbol genealógico

--- arbol_genealogico.pl
+++ arbol_genealogico.pl
@@ -10,7 +10,6 @@ padrede('Andrea','Juan').
 abuelode(A,B):-padrede(A,C),padrede(C,B).
 hijode(A,B):-padrede(B,A).
 tiode(A,B):-abuelode(C,B),hijode(A,C),not(padrede(A,B)).
-%padrede(C,A),padrede(D,B),padrede(C,D).
 bisabuelode(A,B):-padrede(C,B),padrede(D,C),padrede(A,D).
 casadocon(A,B):-padrede(A,C),padrede(B,C),A\==B.
 esfeliz(A):-casadocon(A,_).
